1570a4853574a7020fbf74ee2cb2a00603c2544c
Ricardo Alexandre <ricardo.alexandresa@outlook.com>
2023-07-20 01:43:42 -0300

    changing if to switch

--- Application/Calculator/Implementations/CalculatorApplication.cs
+++ Application/Calculator/Implementations/CalculatorApplication.cs
@@ -13,19 +13,17 @@ namespace Application.Calculator.Implementations
         
         public int SumUpTo(int number, int? previousNumber = null, int sum = 0)
         {
-            if (number == 0)
+            switch (number)
             {
-                _eventError.RaiseValidationError("0 is not an valid number, please input a number bigger than 1 to be fun.", 400);
-                return 0;
+                case 0:
+                    _eventError.RaiseValidationError("0 is not an valid number, please input a number bigger than 1 to be fun.", 400);
+                    return 0;
+                case < 0:
+                    _eventError.RaiseValidationError("Negatives numbers are not valid, please input a number bigger than 1, like your credit card number and the password.", 
+                        statusCode: 400);
+                    return 0;
             }
 
-            if (number < 0)
-            {
-                _eventError.RaiseValidationError("Negatives numbers are not valid, please input a number bigger than 1, like your credit card number and the password.", 
-                    statusCode: 400);
-                return 0;
-            }
-                
             previousNumber ??= 1;
 
             if (number == previousNumber)
